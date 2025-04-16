f64ee2e09b6e191e7c49db840a2ae2b6e452158f
Bohlmeijer, M.W. (Minke, Student M-CS) <m.w.bohlmeijer@student.utwente.nl>
2025-04-16 11:42:43 +0200

    Turned encoder and decoder into functions and fixed the encoding of sam_i_am

--- decoder.py
+++ decoder.py
@@ -1,50 +1,49 @@
 import regex as re
 
-file = open("text_files/encoded/lyric_harderbetterfasterstronger_encoded")
+def decode(filename):
+    file = open(f"text_files/encoded/{filename}_encoded", 'r')
 
-text = str(file.read())
+    text = str(file.read())
 
-decoded_text = ""
+    decoded_text = ""
 
-string_pointer = 0
-while string_pointer < len(text):
-    if text[string_pointer] == "(":
-        string_pointer += 1
-        digits = ""
-        offset = (0, 0)
-        while text[string_pointer].isdigit():
-            digits += text[string_pointer]
+    string_pointer = 0
+    while string_pointer < len(text):
+        if text[string_pointer] == "(":
             string_pointer += 1
-        if digits == "":
-            decoded_text += "("
-            continue
-        offset = (int(digits), 0)
-        if text[string_pointer] == ",":
-            string_pointer += 1
+            digits = ""
+            offset = (0, 0)
+            while text[string_pointer].isdigit():
+                digits += text[string_pointer]
+                string_pointer += 1
+            if digits == "":
+                decoded_text += "("
+                continue
+            offset = (int(digits), 0)
+            if text[string_pointer] == ",":
+                string_pointer += 1
+            else:
+                decoded_text += "(" + digits
+                decoded_text += text[string_pointer]
+                string_pointer += 1
+                continue
+            digits = ""
+            while text[string_pointer].isdigit():
+                digits += text[string_pointer]
+                string_pointer += 1
+            if digits == "":
+                decoded_text += "(" + str(offset[0]) + ","
+                continue
+            offset = (offset[0], int(digits))
+            if text[string_pointer] == ")":
+                decoded_text += decoded_text[len(decoded_text) - offset[0]:(-offset[0] + offset[1] + len(decoded_text))]
+                string_pointer += 1
+            else:
+                decoded_text += "(" + str(offset[0]) + "," + str(offset[1])
+                continue
         else:
-            decoded_text += "(" + digits
             decoded_text += text[string_pointer]
             string_pointer += 1
-            continue
-        digits = ""
-        while text[string_pointer].isdigit():
-            digits += text[string_pointer]
-            string_pointer += 1
-        if digits == "":
-            decoded_text += "(" + str(offset[0]) + ","
-            continue
-        offset = (offset[0], int(digits))
-        if text[string_pointer] == ")":
-            decoded_text += decoded_text[len(decoded_text) - offset[0]:(-offset[0] + offset[1] + len(decoded_text))]
-            string_pointer += 1
-        else:
-            decoded_text += "(" + str(offset[0]) + "," + str(offset[1])
-            continue
-    else:
-        decoded_text += text[string_pointer]
-        string_pointer += 1
-
-print(decoded_text)
-f = open("text_files/decoded/lyric_harderbetterfasterstronger_decoded", 'w')
-f.write(decoded_text)
-f.close()
+    f = open(f"text_files/decoded/{filename}_decoded", 'w')
+    f.write(decoded_text)
+    f.close()
--- encoder.py
+++ encoder.py
@@ -1,52 +1,54 @@
 # Read the file
-file = open("text_files/to_encode/lyric_harderbetterfasterstronger")
 
-input_string = str(file.read())
-file.close()
+def encode_lempel_ziv_string(filename, min_window, look_ahead_size, search_buffer_size):
+    file = open(f"text_files/to_encode/{filename}", 'r')
 
-string_pointer = 0
+    input_string = str(file.read())
+    file.close()
 
-input_had = ""
-output_string = ""
+    string_pointer = 0
 
-min_window = 4
-look_ahead_size = 256 # 32 is the max window size
-search_buffer_size = 256
+    input_had = ""
+    output_string = ""
 
-while string_pointer < len(input_string):
-    looked = False
-    if len(input_had) > search_buffer_size:
-        search_buffer = input_had[len(input_had)-search_buffer_size:len(input_had)]
-    else:
-        search_buffer = input_had
-    if string_pointer == len(input_string) - 1:
-        output_string += input_string[string_pointer]
-        break
-    in_input = string_pointer + min_window
-    if in_input < len(input_string):
-        looking_at = str(input_string[string_pointer:in_input])
-        while looking_at in search_buffer and in_input - string_pointer <= look_ahead_size:
-            looked = True
-            in_input += 1
-            looking_at = input_string[string_pointer:in_input]
-    else:
-        looking_at = str(input_string[string_pointer:])
-    if looked:
-        if in_input <= len(input_string):
-            looking_at = looking_at[:-1]
-            in_input -= 1
-        occurence_index = search_buffer.index(looking_at)
-        length = len(looking_at)
-        output_string += f"({len(search_buffer) - occurence_index},{length})"
-        input_had += looking_at
-        string_pointer = in_input
-    else:
-        output_string += input_string[string_pointer]
-        input_had += input_string[string_pointer]
-        string_pointer += 1
+    min_window = min_window
+    look_ahead_size = look_ahead_size # 32 is the max window size
+    search_buffer_size = search_buffer_size
 
-print(output_string)
-f = open("text_files/encoded/lyric_harderbetterfasterstronger_encoded", 'w')
-f.write(output_string)
+    while string_pointer < len(input_string):
+        looked = False
+        if len(input_had) > search_buffer_size:
+            search_buffer = input_had[len(input_had)-search_buffer_size:len(input_had)]
+        else:
+            search_buffer = input_had
+        if string_pointer == len(input_string) - 1:
+            output_string += input_string[string_pointer]
+            break
+        in_input = string_pointer + min_window
+        if in_input < len(input_string):
+            looking_at = str(input_string[string_pointer:in_input])
+            while looking_at in search_buffer and in_input - string_pointer <= look_ahead_size:
+                looked = True
+                in_input += 1
+                looking_at = input_string[string_pointer:in_input]
+        else:
+            looking_at = str(input_string[string_pointer:])
+        if looked:
+            if in_input <= len(input_string):
+                looking_at = looking_at[:-1]
+                in_input -= 1
+            occurence_index = search_buffer.index(looking_at)
+            length = len(looking_at)
+            output_string += f"({len(search_buffer) - occurence_index},{length})"
+            input_had += looking_at
+            string_pointer = in_input
+        else:
+            output_string += input_string[string_pointer]
+            input_had += input_string[string_pointer]
+            string_pointer += 1
 
-f.close()
+    print(output_string)
+    f = open(f"text_files/encoded/{filename}_encoded", 'w')
+    f.write(output_string)
+
+    f.close()
